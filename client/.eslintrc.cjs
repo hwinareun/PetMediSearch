5cf70e54a9653e6b086e62245cbcb4c03967f270
hwinareun <hwinareun@gmail.com>
2024-09-09 22:11:10 +0900

    FE - init: 초기 패키지 설치 & prettier 적용 #4

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -5,14 +5,28 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+    'plugin:react/jsx-runtime',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect', // 설치된 React 버전을 자동으로 감지
+    },
+  },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': 'off',
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }], // Prettier 규칙을 ESLint 오류로 표시
+    'react/prop-types': 'off', // TypeScript를 사용하므로 prop-types가 필요 없음
+    '@typescript-eslint/explicit-module-boundary-types': 'off', // 함수의 반환 타입을 명시적으로 지정하지 않아도 됨
   },
-}
+};
