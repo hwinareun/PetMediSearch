ff5059dd4fe03e7b46ceb09eb4053bc5443ddddd
hwinareun <hwinareun@gmail.com>
2024-09-10 15:00:43 +0900

    FE - feat: Loading
    - 이후 rottie 구현 고려할 것.
    
    #4

--- client/src/layout/common/Loading.tsx
+++ client/src/layout/common/Loading.tsx
@@ -3,11 +3,22 @@ import styled from 'styled-components';
 function Loading() {
   return (
     <LoadingStyle>
-      <h1>Loading</h1>
+      <img src={'../src/assets/images/Loading.png'} alt="Loading" />
     </LoadingStyle>
   );
 }
 
-const LoadingStyle = styled.div``;
+const LoadingStyle = styled.div`
+  @keyframes rotate {
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+
+  img {
+    width: 70px;
+    animation: rotate 2s linear infinite;
+  }
+`;
 
 export default Loading;
