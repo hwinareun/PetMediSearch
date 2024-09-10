d7dfbbe25fa1636e8e5f9ff545663befafa01622
hwinareun <hwinareun@gmail.com>
2024-09-10 14:51:53 +0900

    FE - feat: Error, Header #4

--- client/src/layout/common/Error.tsx
+++ client/src/layout/common/Error.tsx
@@ -10,13 +10,42 @@ function Error() {
   const error = useRouteError() as RouteError;
   return (
     <ErrorStyle>
-      <h1>Error!</h1>
-      <p>{error.statusText}</p>
-      <p>{error.message}</p>
+      <img src={'../src/assets/images/Error.png'} />
+      <p className="error">
+        <h3>Error!</h3>
+        <h6>{error.message}</h6>
+      </p>
+      <a href="/">메인으로 이동</a>
     </ErrorStyle>
   );
 }
 
-const ErrorStyle = styled.div``;
+const ErrorStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  img {
+    width: 200px;
+  }
+  p {
+    border-top: solid black;
+    border-bottom: solid black;
+    padding: 10px;
+    font-size: 20px;
+    text-align: center;
+  }
+
+  a {
+    font-size: 10px;
+    color: grey;
+    text-decoration: none;
+  }
+
+  a:hover {
+    color: black;
+    text-decoration: none;
+  }
+`;
 
 export default Error;
--- client/src/layout/common/Header.tsx
+++ client/src/layout/common/Header.tsx
@@ -1,13 +1,31 @@
 import styled from 'styled-components';
+import Menu from './Menu';
 
 function Header() {
   return (
     <HeaderStyle>
-      <h1>Header</h1>
+      <a href="/">
+        <img src={'../src/assets/images/Icon.png'} />
+      </a>
+      <Menu />
     </HeaderStyle>
   );
 }
 
-const HeaderStyle = styled.div``;
+const HeaderStyle = styled.div`
+  display: flex;
+  padding-top: 10px;
+  width: 100%;
+  height: 50px;
+  justify-content: space-between;
+  align-items: center;
+  padding-bottom: 5px;
+  border-bottom: solid;
+
+  img {
+    width: 60px;
+    padding: 10px;
+  }
+`;
 
 export default Header;
