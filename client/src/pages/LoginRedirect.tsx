6a93bec2525419408874074794539fe9a71335d8
hwinareun <hwinareun@gmail.com>
2024-09-11 19:13:19 +0900

    FE - feat: 카카오 인가코드 백엔드로 보내는 로직 추가
     #9 #10

--- client/src/pages/LoginRedirect.tsx
+++ client/src/pages/LoginRedirect.tsx
@@ -1,10 +1,32 @@
 import styled from 'styled-components';
 import Loading from '../layout/common/Loading';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 function LoginRedirectKakao() {
+  const navigate = useNavigate();
+  // 인가코드 가져오기
+  const code = new URL(window.location.href).searchParams.get('code');
+
+  useEffect(() => {
+    const kakaoLogin = async () => {
+      await axios({
+        method: 'GET',
+        url: `${import.meta.env.VITE_REDIRECT_URL}/?code=${code}`,
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8', //json형태로 데이터를 보내겠다는 의미
+        },
+      }).then((res) => {
+        console.log(res);
+        navigate('/'); // 로그인 성공 시 메인 페이지로 이동
+      });
+    };
+    kakaoLogin();
+  }, []);
+
   return (
     <LoginRedirectKakaoStyle>
-      {/* 인가코드 백으로 넘기는 로직 구현 필요 */}
       <Loading />
       <p>로그인 중입니다.</p>
       <p>잠시만 기다려주세요.</p>
