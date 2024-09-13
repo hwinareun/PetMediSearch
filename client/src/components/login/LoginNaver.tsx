aa67dad4a8092ed8ab5b2b7979f133cefd6823fb
hwinareun <hwinareun@gmail.com>
2024-09-13 20:17:52 +0900

    FE - feat: create LoginNaver.tsx
     #9 #14

---
+++ client/src/components/login/LoginNaver.tsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const N_CLIENT_ID = import.meta.env.VITE_N_REST_API_KEY;
+const N_REDIRECT_URI = import.meta.env.VITE_N_REDIRECT_URL;
+
+const NAVER_URL = `nid.naver.com/oauth2.0/authorize?client_id=${N_CLIENT_ID}&redirect_uri=${N_REDIRECT_URI}&response_type=code`;
+
+function LoginNaver() {
+  return (
+    <LoginNaverStyle>
+      <a href={NAVER_URL} className="naverbttn">
+        <img src={`../src/assets/images/login/LoginNaver.png`} />
+      </a>
+    </LoginNaverStyle>
+  );
+}
+
+const LoginNaverStyle = styled.div``;
+
+export default LoginNaver;
