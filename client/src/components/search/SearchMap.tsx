ad9e682c66b84ff73e051d7648b363072dc5ab6c
hwinareun <hwinareun@gmail.com>
2024-09-19 23:06:48 +0900

    (FE) [feat] 지도 생성
     #26 #29

--- client/src/components/search/SearchMap.tsx
+++ client/src/components/search/SearchMap.tsx
@@ -1,9 +1,30 @@
+import { Map, useKakaoLoader } from 'react-kakao-maps-sdk';
 import styled from 'styled-components';
+import Loading from '../common/Loading';
+import { useEffect } from 'react';
 
 function SearchMap() {
+  const [loading, error] = useKakaoLoader({
+    appkey: import.meta.env.VITE_K_JAVASCRIPT_KEY,
+  });
+
+  useEffect(() => {
+    if (error) {
+      console.error('ReactKakaoMapSDK 로드 중 오류 발생:', error);
+    }
+  }, [error]);
+
   return (
     <SearchMapStyle>
-      <h1>SearchMap</h1>
+      {loading ? (
+        <Loading />
+      ) : (
+        <Map
+          center={{ lat: 33.450701, lng: 126.570667 }} // 초기 위치
+          style={{ width: '600px', height: '600px' }} // 지도 크기 설정
+          level={3} // 지도 확대 레벨
+        />
+      )}
     </SearchMapStyle>
   );
 }
