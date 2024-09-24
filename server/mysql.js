74188e545bd44bacae6c2dd0392a7b40186dfe67
hwinareun <hwinareun@gmail.com>
2024-09-24 22:13:02 +0900

    (BE) [chore] DB 패스워드 원래대로 변경
    - 제 로컬db 패스워드와 달라서 오류가 나서 일시적으로 바꿈.
    - 코드 구현 완료 하던 중 같이 커밋 되어 다시 원래대로 돌려 커밋함.

--- server/mysql.js
+++ server/mysql.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const conn = mysql.createConnection({
   host: 'localhost', 
   user: 'root',
-  password: 'mysql',
+  password: 'password',
   database: 'petmedisearch',
   dateStrings: true
 });
