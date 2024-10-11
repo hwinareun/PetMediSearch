9b24f525dc3dadd9c150ff0ec9fb58ce6cfe3603
DahyeY <dhuniv@naver.com>
2024-10-11 19:08:49 +0900

    (BE) [feat] 카테고리 리스트 불러오는 api 구현

--- server/controller/category.js
+++ server/controller/category.js
@@ -1,18 +1,34 @@
 const conn = require('../mysql');
+const getCategories = (req, res) => {
+
+}
 
 const getListByCategory = (req, res) => {
     const categoryId = req.query.category;
-    console.log(categoryId)
     if (!categoryId) {
-        return res.status(400).send({ message: '카테고리 ID가 필요합니다.' });
+        const query = `
+        SELECT * FROM categories
+    `;
+
+        conn.query(query, (error, results) => {
+            if (error) {
+                return res.status(500).send({ message: '서버 오류 발생', error });
+            }
+            return res.send(results);
+        });
     }
 
-    const category_id = parseInt(categoryId, 10);
-    if (isNaN(category_id)) {
-        return res.status(400).send({ message: '유효한 카테고리 ID가 필요합니다.' });
-    }
+    else {
+        if (!categoryId) {
+            return res.status(400).send({ message: '카테고리 ID가 필요합니다.' });
+        }
 
-    const query = `
+        const category_id = parseInt(categoryId, 10);
+        if (isNaN(category_id)) {
+            return res.status(400).send({ message: '유효한 카테고리 ID가 필요합니다.' });
+        }
+
+        const query = `
         SELECT p.post_id, p.title, p.content, p.created_at, u.username
         FROM posts p
         JOIN users u ON p.user_id = u.user_id
@@ -20,14 +36,16 @@ const getListByCategory = (req, res) => {
         ORDER BY p.created_at DESC
     `;
 
-    conn.query(query, [category_id], (error, results) => {
-        if (error) {
-            return res.status(500).send({ message: '서버 오류 발생', error });
-        }
-        return res.send({ posts: results });
-    });
+        conn.query(query, [category_id], (error, results) => {
+            if (error) {
+                return res.status(500).send({ message: '서버 오류 발생', error });
+            }
+            return res.send({ posts: results });
+        });
+    }
 }
 
 module.exports = {
+    getCategories,
     getListByCategory
 }
