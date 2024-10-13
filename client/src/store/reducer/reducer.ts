57f197450390f17b250f7ae55fb26f13e7227058
hwinareun <hwinareun@gmail.com>
2024-10-14 02:35:07 +0900

    (FE) [remove] reviewSlice 삭제

--- client/src/store/reducer/reducer.ts
+++ client/src/store/reducer/reducer.ts
@@ -1,11 +1,9 @@
 import { authReducer } from '../slices/authSlice';
 import { placeReducer } from '../slices/placeSlice';
-import { reviewReducer } from '../slices/reviewSlice';
 
 const reducer = {
   auth: authReducer,
   place: placeReducer,
-  review: reviewReducer,
 };
 
 export default reducer;
