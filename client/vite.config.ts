91546bca6fe4d27b12ab11c6b8ccdde5f16d83b0
hwinareun <hwinareun@gmail.com>
2024-10-17 09:24:44 +0900

    (FE) [fix] 클라우드타입으로 배포 시도

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5000,
+  build: {
+    outDir: 'dist', // 빌드 결과물을 dist 폴더에 저장
+    rollupOptions: {
+      input: './src/main.tsx', // 진입 파일 설정
+    },
   },
 });
